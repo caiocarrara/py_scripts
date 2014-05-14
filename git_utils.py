591aea8309b11f3803a7bf97217f134a10003e17
Caio Carrara <caiocarrara@gmail.com>
2014-05-13 23:23:17 -0300

    Converting tabs to spaces

--- git_utils.py
+++ git_utils.py
@@ -4,33 +4,33 @@ import subprocess
 from sys import argv
 
 def git_all_subdir(base_dir=".", arguments=[]):
-	base_dir_path = os.path.abspath(base_dir)
-	for dir in os.listdir(base_dir_path):
-		dir_path = os.path.join(base_dir_path, dir)
-		if os.path.isdir(dir_path) and not dir.startswith("."):
-			if ".git" in os.listdir(dir_path):
-				command = ['git', '-C', dir_path]
-				command.extend(arguments)
-				subprocess.call(command)
+    base_dir_path = os.path.abspath(base_dir)
+    for dir in os.listdir(base_dir_path):
+        dir_path = os.path.join(base_dir_path, dir)
+        if os.path.isdir(dir_path) and not dir.startswith("."):
+            if ".git" in os.listdir(dir_path):
+                command = ['git', '-C', dir_path]
+                command.extend(arguments)
+                subprocess.call(command)
 
 if __name__ == "__main__":
-	"""
-	Call git_utils.py passing arguments as passing to a git command.
-	For exemple, to call 'git pull' in all subdirectories, execute
-	'python git_utils.py pull origin master'. Or, specify a directory to
-	execute command. Exemple: 
+    """
+    Call git_utils.py passing arguments as passing to a git command.
+    For exemple, to call 'git pull' in all subdirectories, execute
+    'python git_utils.py pull origin master'. Or, specify a directory to
+    execute command. Exemple: 
 
-	python git_utils.py base_dir=../path pull origin master
-	"""
-	base_dir = "."
-	base_dir_index = None
+    python git_utils.py base_dir=../path pull origin master
+    """
+    base_dir = "."
+    base_dir_index = None
 
-	for arg in argv:
-		if arg.startswith('base_dir='):
-			base_dir_index = argv.index(arg)
-			base_dir = arg.split('=')[1]
+    for arg in argv:
+        if arg.startswith('base_dir='):
+            base_dir_index = argv.index(arg)
+            base_dir = arg.split('=')[1]
 
-	if base_dir_index:
-		argv.pop(base_dir_index)
+    if base_dir_index:
+        argv.pop(base_dir_index)
 
-	git_all_subdir(base_dir=base_dir, arguments=argv[1:])
+    git_all_subdir(base_dir=base_dir, arguments=argv[1:])
--- replace_in_all_files.py
+++ replace_in_all_files.py
@@ -3,35 +3,35 @@ import os
 from sys import argv
 
 def replace_in_all_files(parent_folder_path, oldChar, newChar):
-	"""Replace the oldChar for newChar in all text
-	files found in parent_folder_path and its children
-	directories.
-	"""
-	filenames = os.listdir(parent_folder_path)
-	
-	for filename in filenames:
-		filename = os.path.join(parent_folder_path, filename)
-		
-		if os.path.isdir(filename):
-			replace_in_all_files(filename, oldChar, newChar)
-		else:
-			arq = open(os.path.join(parent_folder_path, filename), 'r')
-			oldContent = arq.readlines()
-			arq.close()
-			
-			arq = open(os.path.join(parent_folder_path, filename), 'w')
-			newContent = ""
-			for line in oldContent:
-				newContent += line.replace(oldChar, newChar) 
-			arq.write(newContent)
-			arq.close()
-			print "OK: " + filename
+    """Replace the oldChar for newChar in all text
+    files found in parent_folder_path and its children
+    directories.
+    """
+    filenames = os.listdir(parent_folder_path)
+    
+    for filename in filenames:
+        filename = os.path.join(parent_folder_path, filename)
+        
+        if os.path.isdir(filename):
+            replace_in_all_files(filename, oldChar, newChar)
+        else:
+            arq = open(os.path.join(parent_folder_path, filename), 'r')
+            oldContent = arq.readlines()
+            arq.close()
+            
+            arq = open(os.path.join(parent_folder_path, filename), 'w')
+            newContent = ""
+            for line in oldContent:
+                newContent += line.replace(oldChar, newChar) 
+            arq.write(newContent)
+            arq.close()
+            print "OK: " + filename
 
 if __name__ == "__main__":
-	if len(argv) < 3:
-		print "You need to inform all params: rootPath, oldChar, newChar"
-	else:
-		rootPath = str(argv[1])
-		oldChar = str(argv[2])
-		newChar = str(argv[3])
-		replace_in_all_files(rootPath, oldChar, newChar)
+    if len(argv) < 3:
+        print "You need to inform all params: rootPath, oldChar, newChar"
+    else:
+        rootPath = str(argv[1])
+        oldChar = str(argv[2])
+        newChar = str(argv[3])
+        replace_in_all_files(rootPath, oldChar, newChar)
